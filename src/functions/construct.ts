df8d5ee65ded7b0b262022bdd7aa5cb660ba8111
Raghav Misra <raghav.m2014@gmail.com>
2020-07-22 14:57:42 -0700

    Finish construct, testing time

--- src/functions/construct.ts
+++ src/functions/construct.ts
@@ -1,28 +1,51 @@
-import { RenderResult } from "../functions/render";
-
-/* Type of a watcher function */
-export type WatcherCallback = (
-    this: HTMLElement,
-    newValue: string,
-    oldValue: string
-) => void;
-
-/* Main configuration object */
-export interface IWebElementConfig {
-    /* Lifecycle hooks */
-    created?(this: HTMLElement): void; // Called in "constructor" right after "super()".
-    mounted?(this: HTMLElement): void; // Called in "connectedCallback".
-    removed?(this: HTMLElement): void; // Called in "disconnectedCallback".
-    adopted(this: HTMLElement): void; // Called in "adoptedCallback".
-
-    /* Watch attributes */
-    watch?: Record<string, WatcherCallback>; // Called in "attributeChangedCallback".
-
-    /* Render function returns children */
-    render(this: HTMLElement): RenderResult;
-}
+import { IWebElementConfig } from "../internals/api";
+import { render } from "../functions/render";
 
 /* Construct a custom element: */
 export function construct(tagName: string, config: IWebElementConfig): void {
 
+    const Component = class extends HTMLElement {
+        static get observedAttributes() { return Object.keys(config.watch || Object.create(null)); }
+
+        constructor() {
+            // Required super call.
+            super();
+            
+            // Call "created" lifecycle hook:
+            config.created && config.created.call(this);
+
+            // Register event handlers:
+            if (config.events) {
+                for (const eventName in config.events) {
+                    this.addEventListener(eventName, config.events[eventName].bind(this));
+                }
+            }
+
+            // Append children:
+            config.render && render((config.render.bind(this))(), this);
+        }
+
+        connectedCallback() {
+            // Call "mounted" lifecycle hook:
+            config.mounted && config.mounted.call(this);
+        }
+
+        disconnectedCallback() {
+            // Call "mounted" lifecycle hook:
+            config.removed && config.removed.call(this);
+        }
+
+        adoptedCallback() {
+            // Call "mounted" lifecycle hook:
+            config.adopted && config.adopted.call(this);
+        }
+
+        attributeChangedCallback(name: string, newValue: string, oldValue: string) {
+            // Call watcher function for changed attribute:
+            config.watch && config.watch[name].call(this, newValue, oldValue);
+        }
+    };
+
+    // Register element:
+    customElements.define(tagName, Component);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 export { h } from "./functions/h";
+export { render, RenderResult } from "./functions/render";
+export { construct } from "./functions/construct";
+
+export {
+	IWebElementConfig,
+	WatcherCallback,
+	EventHandler,
+} from "./internals/api";
